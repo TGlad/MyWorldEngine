1c6bae585e29f631f0f23fc8d73072555f56e551
Tom Lowe <t_d_lowe@hotmail.com>
2014-10-31 18:52:35 +1000

    Small definition changes

--- PTree.h
+++ PTree.h
@@ -1,23 +1,42 @@
 #include "standardIncludes.h"
-typedef Matrix<double, 6, 1> Vector6d;
-typedef Matrix<double, 6, 6> PMatrix;
+typedef Matrix<double, 4, 4> PMatrix;
 
-struct PVector
+struct Extents
 {
-  Vector3d position;
-  Vector3d velocity;
-
-  PVector(const Vector3d &position, const Vector3d &velocity) : position(position), velocity(velocity) {}
-  Vector6d vec(){ Vector6d vec; vec << position, velocity; return vec; }
+  vector3d position;
+  vector3d velocity;
 };
+struct Contact;
 
-
-// implements a parallelotope tree, i.e. 6d boxes with transforms
+// implements a parallelotope tree, i.e. 6d boxes with transforms. Each node's data is relative to its parent
 struct PNode
 {
-  PVector extents; // box-like extents
-  PVector state; // position and velocity
-  PMatrix transform;
+  PNode(const Vector3d &pos, const Quaternion &rot, const Extents &extents, const Vector3d &vel, const Vector3d &angVel) : position(pos), rotation(rot), extents(extents), velocity(vel), angularVelocity(angVel) {}
+
+  Vector3d position;
+  Quaternion rotation;
+  Extents extents; // box-like extents 
+  Vector3d velocity, angularVelocity;
+
+  // converts state data into a single transform matrix, for concatenating
+  PMatrix getTransform()
+  {
+    PMatrix mat;
+    mat.identity();
+    mat.block(0,3,3,3) = position;
+    mat.block(0,0,2,2) = rotation.toMatrix();
+    // now boost the matrix here
+    return mat;
+  }
 
   vector<PNode> children;
+  vector<Contact> contacts;
+};
+
+struct Contact
+{
+  PNode *nodes[2];
+  Vector3d normal;
+  double depth;
+  double depthVelocity;  // together these give a time and speed of impact
 };
